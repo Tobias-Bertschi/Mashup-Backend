25b96220677de64110b5fcf981cc4fba0fece94c
tobiasbertschi <gotteron89tb@gmail.com>
2022-12-22 09:41:49 +0100

    HoroscopeService

--- Mashup-Backend/src/main/java/ch/bbw/mashupbackend/service/HoroscopeService.java
+++ Mashup-Backend/src/main/java/ch/bbw/mashupbackend/service/HoroscopeService.java
@@ -1,14 +1,29 @@
 package ch.bbw.mashupbackend.service;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class HoroscopeService {
 
     private String url = "https://aztro.sameerkumar.website/?sign=scorpio&day=today";
 
     public String createHoroscope() {
-        return "";
+
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String test = "";
+            ResponseEntity<String> response = restTemplate.postForEntity(url, test, String.class);
+
+            return response.getBody().toString();
+        } catch (
+                Exception e) {
+            System.out.println(e);
+        }
+
+        return "fail";
+
     }
 
     public String getUrl() {
